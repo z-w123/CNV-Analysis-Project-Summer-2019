e6991beca31513c844cda1939eb0e16736d71977
z-w123 <45365618+z-w123@users.noreply.github.com>
2019-08-24 19:32:22 +0100

    Update and rename 4_final_Bedtools_bash_script.bash to Bash_Script_4.sh

--- Bash_Script_4.sh
+++ Bash_Script_4.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
 
-GENELIST_BED="Biomart_Genes_Exons_List_Obesity.bed"   #desired virtual panel list in bed format - do not need to set filepath if u r already in working directory
+GENELIST_BED="Biomart_Genes_Exons_List_Obesity.bed"   
 SAMPLECNVS_BED=`ls /homes/athosnew/Genetics_Centre_Bioinformatics/CNV_calling_zahra/test_extra_samples/*.csv.bed`  
 echo $SAMPLECNVS_BED
 
 #mkdir /homes/athosnew/Genetics_Centre_Bioinformatics/CNV_calling_zahra/test_extra_samples/test_extra_samples_BEDtools
 
+#with the BEDtools intersect function, if you get an empty excel file output, it is because there is simply no overlap between the two files.
 
-#now bedtools command:
-#remember if you get an empty excel file output, it is bc there is simply no overlap between the two files. csv->BED script has been tested and should be correct.
-#can also try the perl command  perl -p -i -e 's/ /\t/g'  bed_file.txt  on the GENELIST_BED file 
 for sample in $SAMPLECNVS_BED; do
 
 
@@ -17,7 +15,7 @@ for sample in $SAMPLECNVS_BED; do
 	
 	head ${sample}_tmp
 	
-	awk '{print $1,$2,$3,$8}' ${sample}_tmp > ${sample}_tmp2	#should extract sample CNV start pos, end pos and gene name & save it as the name of that sample file in csv form (i.e w/o .bed extension)    
+	awk '{print $1,$2,$3,$8}' ${sample}_tmp > ${sample}_tmp2	#to extract sample CNV start pos, end pos and gene name 
 	
 	
 	mv ${sample}_tmp2 `basename $sample .csv.bed`_BEDtoolsoverlaps_known_genes.csv;
@@ -26,8 +24,6 @@ for sample in $SAMPLECNVS_BED; do
 
 done
 
-#find . -name '*_BEDtoolsoverlaps_known_genes.csv' -type f -empty -delete                     #need to remove empty files 
-#when I extract columns I might not see the header of each column - is it poss to code this back in? 
 
 #if you want to display which part of the exome csv bed file overlapped with which part of the candidate gene list. Outputs the genes from gene list too:
 #/homes/athosnew/Genetics_Centre_Bioinformatics/resourses/bedtools2/bin/intersectBed -a $SAMPLECNVS_BED -b $GENELIST_BED -wa -wb | head -5
@@ -39,4 +35,3 @@ done
 #/homes/athosnew/Genetics_Centre_Bioinformatics/resourses/bedtools2/bin/intersectBed -a $SAMPLECNVS_BED -b $GENELIST_BED -c
 																		#or
 #/homes/athosnew/Genetics_Centre_Bioinformatics/resourses/bedtools2/bin/intersectBed -a $GENELIST_BED -b $SAMPLECNVS_BED -c
-#mv ${sample}_tmp2 ${sample}_BEDtoolsoverlaps_known_genes.csv
