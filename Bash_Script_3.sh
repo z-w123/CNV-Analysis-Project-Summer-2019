80ca5c8a51f593a36b514e0cf2d2a4bef42bb9cb
z-w123 <45365618+z-w123@users.noreply.github.com>
2019-08-24 19:25:38 +0100

    Update and rename 3_Biomart_gene_list_to_bed_script.bash to Bash_Script_3.sh

--- Bash_Script_3.sh
+++ Bash_Script_3.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/bash
 
-#once you have virtual gene panel in a tsv file format from Biomart, you could:
-# (a) ensure that for your GENELIST_BED file you have already used excel to manually remove the header and arrange your columns like so: Chrom, Start, Stop, Gene Name, . 
-#or (b) do this in linux, like so: 
+#the following code converts the virtual gene panel in tsv file from Biomart, into BED file format, for later use with BEDtools
+ 
 
-GENELIST_BED="Biomart_Genes_Exons_List_Obesity.bed"   #desired virtual panel list in bed format - do not need to set filepath if u r already in working directory
+GENELIST_BED="Biomart_Genes_Exons_List_Obesity.bed"   #desired virtual panel list in bed format - do not need to set filepath if u are already in working directory
 SAMPLECNVS_BED=`ls /homes/athosnew/Genetics_Centre_Bioinformatics/CNV_calling_zahra/test_samples/*.csv.bed`  
 echo $SAMPLECNVS_BED
 
-cat $GENELIST_BED | tr ',' '\t' > ${GENELIST_BED}_tmp         #do I need to specify file directory here?
+cat $GENELIST_BED | tr ',' '\t' > ${GENELIST_BED}_tmp         
 awk '{print $2,$3,$4,$1}' ${GENELIST_BED}_tmp > ${GENELIST_BED}_tmp2       #shuffling columns to satisfy BED format
 sed 1d ${GENELIST_BED}_tmp2 > ${GENELIST_BED}_tmp3         #removing header
 sed 's/ /\t/g' ${GENELIST_BED}_tmp3 > ${GENELIST_BED}_tmp4
