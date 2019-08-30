599d6ed1d5bda8c1ec6798e33a880b3ab006f543
z-w123 <45365618+z-w123@users.noreply.github.com>
2019-08-31 00:46:29 +0100

    Update bedtools_hm_maps_with_corresp_annot_snps.bash

--- Homozygosity Analysis/bedtools_hm_maps_with_corresp_annot_snps.bash
+++ Homozygosity Analysis/bedtools_hm_maps_with_corresp_annot_snps.bash
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
-#making a script to run through each HM_sample and intersect it with the CORRESPONDING annot.snps.exome file
+#A script to run through each homozygosity map (HM_sampleID) and intersect it with the CORRESPONDING annotated annovar file for that sample 
 
+#below just shows path directory to: 1) homozygosity maps of consanguinous patients
+#and 
+#2) patient SNVs in monogenic obesity panelapp genes 
 
 #HOMOZYGOSITY_MAPS_CONSANG=`ls /homes/homedirs34/sghms/student/users/p1806425/test_MO43_MO65_CNV/Homozygosity_Mapper/ED_csv_bed_hm/*_analysis.bed`
-#PANELAPP_PATIENT_SNPS=`ls /homes/homedirs34/sghms/student/users/p1806425/test_MO43_MO65_CNV/Homozygosity_Mapper/ED_csv_bed_hm/*_panelapp_genes.bed`
+#PANELAPP_PATIENT_SNVS=`ls /homes/homedirs34/sghms/student/users/p1806425/test_MO43_MO65_CNV/Homozygosity_Mapper/ED_csv_bed_hm/*_panelapp_genes.bed`
 
 #add header to output file!
-#just created the header file in excel for this one, with the following columns in this order: #CHROM, #STAR, #STOP, CHROM, START, STOP, ID [where # denotes data from the homozygosity maps] and saved as text file: header.txt
+#first created the header file in excel, with the following columns in this order: #CHROM, #STAR, #STOP, CHROM, START, STOP, ID [where # denotes data from the homozygosity maps] and saved as text file: header.txt
 
 mySamples="MO46 MO47 MO49 MO55 MO58"
 
@@ -21,19 +24,3 @@ for sample in $mySamples; do
 	
 done
 
-
-#for map in $HOMOZYGOSITY_MAPS_CONSANG; do
-
-	#sample=${map%_HM_analysis.bed}
-
-	#cat header.txt > ${map}.csv
-		
-	#/homes/athosnew/Genetics_Centre_Bioinformatics/resourses/bedtools2/bin/intersectBed -a $map -b $sample.annovar.annot_snps_panelapp_genes.bed -wa -wb >> ${map}.csv
-		
-	#done
-	
-	#perl -p -i -e 's/\t/,/g' ${map}.csv
-	
-	#mv ${map}.csv `basename $map .bed`.overlap_exomedepth_calls.csv;
-	
-#done
